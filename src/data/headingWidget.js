c5d2cb9285d860936699cd084fa7d8f6ba8f1149
Orest Matuskh <orest.sd@gmail.com>
2025-07-09 10:53:22 +0300

    Enhance headingWidget.js, segmentedToggleTabs.js, and spacing.js by adding new widget data entries and corresponding code snippets for improved functionality and visual representation.

--- src/data/headingWidget.js
+++ src/data/headingWidget.js
@@ -25,7 +25,27 @@ export const headingWidgetData = {
       code: `
 @include('widget::components.design-parts.widget_header', [
     'w_icon' => 'widgetName.svg',
-    'w_title' => 'widgetName,
+    'w_title' => 'widgetName',
+])
+    `
+    },
+    {
+      id: '2',
+      image: `${process.env.PUBLIC_URL}/assets/images/headingWidget/2.png`,
+      code: `
+@include('widget::components.design-parts.widget_header', [
+    'w_btn_class' => 'js-repeater-field__content-btn',
+    'w_title' => $pre . '_title',
+])
+    `
+    },
+    {
+      id: '3',
+      image: `${process.env.PUBLIC_URL}/assets/images/headingWidget/3.png`,
+      code: `
+@include('widget::components.design-parts.widget_header', [
+    'w_btn_class' => 'js-repeater-field__content-btn-inner',
+    'w_title' => $pre . '_item_inner_title',
 ])
     `
     },
